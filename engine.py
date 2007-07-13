2ce2c958a5c577089822759a0ea4e13bfbbd7038
shiblon <shiblon@e9066bae-eb34-0410-adca-61464099497e>
2007-07-13 01:54:33 +0000

    Added some more skeleton code
    git-svn-id: https://hagoth.googlecode.com/svn/trunk@5 e9066bae-eb34-0410-adca-61464099497e

--- engine.py
+++ engine.py
@@ -4,12 +4,49 @@ Implements the main logic engine for Hagoth.
 
 """
 
+import re
+
 class Predicate(object):
     def __init__( self, name, args=() ):
-        assert name
-
         self.name = name
         self.args = args
+
+class Var(object):
+    def __init__( self, name, constraint=re.compile(".*") ):
+        """Create a new variable with the given name and constraints
+
+        args:
+            name - name of the variable
+            constraint - regular expression intended to match the entire value.
+                If the constraint doesn't match, assignment is impossible.
+        """
+        self.name = name
+        self.regex = regex
+
+class Assignment(object):
+    """A variable assignment containing a variable and its value
+    
+    Holds state pertinent to that assignment, like submatches in the
+    constraints.  Currently just implemented as a regex with groups, but a more
+    general implementation is needed.
+    """
+
+    def __init__( self, var, value ):
+        """Creates an assigned variable
+
+        args:
+
+            var - a Var object
+            value - an assignment to that var (cannot be another variable, that
+                sort of thing is handled by the engine in a special way)
+        """
+        self.var = var
+        self.value = value
+        # A full match is expected -- no part of the variable may be left
+        # unmatched by the regular expression.  This ensures, among other
+        # things, that the group assignments are maximally predictable.
+        self.match = var.regex.match(value)
+        assert self.match is not None
 
 class Rule(object):
     def __init__( self, consequent, antecedents ):
@@ -19,13 +56,31 @@ class Rule(object):
 
         assert self.consequent.name + "Rule" == self.name
 
+    def concrete_rule( self, assignments ):
+        """Returns a rule of the same type, but with values concretized.
+        
+        The consequents and antecedents of the new rule will be variable-free
+        (if possible, otherwise raises an exception)
+        """
+        # TODO
+        pass
+
     def test( self, assignments ):
+        """Returns true if, given satisfied antecedents, the rule is 'true'
+        """
         raise NotImplementedError()
 
     def commands( self, assignments ):
+        """Commands that are executed to make the rule true.
+
+        These are executed when the antecedents are satisfied, but the test
+        still fails.  The test is run after these to determine ultimate
+        satisfiability of the rule.
+        """
         raise NotImplementedError()
 
 class FileIsCurrentRule(Rule):
+    # TODO: test and commands
     pass
 
 
@@ -49,5 +104,12 @@ class Engine(object):
         #       Succeed (for this rule)
 
         pass
+
+    # TODO:
+    #   - Unification
+    #   - Substitution
+    #   - Resolution
+    #   - Special handling of variable-to-variable mappings and assignment
+    #       collapsing when appropriate.
 
 # vim: et sts=4 sw=4
