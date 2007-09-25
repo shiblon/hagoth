3d35e55d7a523c906f6f73e8f55a8a027d2b3a59
shiblon <shiblon@e9066bae-eb34-0410-adca-61464099497e>
2007-09-25 12:33:24 +0000

    The unification algorithm now works and passes the doc tests
    git-svn-id: https://hagoth.googlecode.com/svn/trunk@14 e9066bae-eb34-0410-adca-61464099497e

--- testdecorators.py
+++ testdecorators.py
@@ -24,6 +24,250 @@ things that the function might need.
 rule_list = []
 rule_map = {}
 
+def is_var( v ) :
+    # TODO: Make this more robust (use a real variable representation)
+    return len(v) > 1
+
+
+class PathInfo(object):
+    def __init__( self, matchtype, numpaths ):
+        self.type = matchtype
+        self.paths = numpaths
+
+    def __str__( self ):
+        return '%s%d' % (self.type, self.paths)
+
+    def __repr__( self ):
+        return repr(str(self))
+
+class MatchMatrix(object):
+    def __init__( self, s1, s2 ):
+        """ Build a match matrix from the input strings
+
+            . : constant-constant match
+            r : row var match
+            c : col var match
+            * : row/col var match (equivalence)
+
+            Rules (everything must match at least one other thing, no empty
+            matches):
+              - 0 if not a simple match
+              - value is sum of inherited values
+              - all matches can inherit from upper left diagonal
+              - r match may inherit r-match from left
+              - c match may inherit c-match from above
+              - . match inherits from upper left only
+
+            The number at the bottom right is the number of matches.
+
+        >>> print MatchMatrix("a{b}c{d}e", "abccc{e}e")
+        .1 -0 -0 -0 -0 -0 -0
+        -0 r1 r1 r1 r1 r1 r1
+        -0 -0 .1 .1 .1 c1 -0
+        -0 -0 -0 r1 r2 *4 r5
+        -0 -0 -0 -0 -0 c6 .4
+        >>> print MatchMatrix("a{b}{c}def", "abc{d}{e}f")
+        .1 -0 -0 -0 -0 -0
+        -0 r1 r1 r1 r1 r1
+        -0 -0 r1 r2 r3 r4
+        -0 -0 -0 c1 c2 -0
+        -0 -0 -0 c1 c3 -0
+        -0 -0 -0 c1 c4 .3
+        """
+        # TODO: Standardize apart?
+        ps1 = self.ps1 = parse_match_string(s1)
+        ps2 = self.ps2 = parse_match_string(s2)
+
+        matrix = [[PathInfo('-', 0) for col in ps2] for row in ps1]
+        self.matrix = matrix
+        self.num_rows = len(matrix)
+        self.num_cols = len(matrix[0])
+
+        # Start at the upper left corner and make our way down to the lower
+        # right.  Assume that the corner matches.
+        for row, rval in enumerate(ps1):
+            for col, cval in enumerate(ps2):
+                info = matrix[row][col]
+                diag_source = 0
+                # Diagonal is always allowed.  It's also always 1 if we are in
+                # the upper left corner.
+                if col == 0 and row == 0:
+                    diag_source = 1
+                elif col > 0 and row > 0:
+                    diag_source = matrix[row-1][col-1].paths
+
+                # If we have a constant match, that is the most constrained, so
+                # we handle it before doing anything else.
+                if not is_var(rval) and not is_var(cval) and rval == cval:
+                    info.type = '.'
+                    info.paths = diag_source
+                    # We can't have variables here, so just skip to the next
+                    # one
+                    continue
+
+                # If we get this far, we have a variable somewhere
+                rvar_source = 0
+                cvar_source = 0
+
+                # Row vars can only inherit from left if also a row var
+                if is_var(rval) and col > 0 and matrix[row][col-1].type in 'r*':
+                    rvar_source = matrix[row][col-1].paths
+                # Col vars can only inherit from above if also a col var
+                if is_var(cval) and row > 0 and matrix[row-1][col].type in 'c*':
+                    cvar_source = matrix[row-1][col].paths
+
+                # If we have two variables, we choose which version we care
+                # about based on the surrounding values.
+                if is_var(rval) and is_var(cval):
+                    if row == 0 and col == 0:
+                        info.type = '*'
+                        info.paths = 1
+                    elif rvar_source and cvar_source:
+                        info.type = '*'
+                        info.paths = diag_source + rvar_source + cvar_source
+                    elif rvar_source:
+                        info.type = 'r'
+                        info.paths = diag_source + rvar_source
+                    elif cvar_source:
+                        info.type = 'c'
+                        info.paths = diag_source + cvar_source
+                elif is_var(rval):
+                    info.type = 'r'
+                    if row == 0 and col == 0:
+                        info.paths = 1
+                    else:
+                        info.paths = diag_source + rvar_source
+                elif is_var(cval):
+                    info.type = 'c'
+                    if row == 0 and col == 0:
+                        info.paths = 1
+                    else:
+                        info.paths = diag_source + cvar_source
+
+                if info.paths == 0:
+                    info.type = '-'
+
+    def paths( self, path=None ):
+        """ Get all paths through the matrix that end in "path"
+
+            To produce the actual matches, we start at the bottom and work up:
+            - From any match, move diagonally
+            - From a 'r' or '*' match, move left if 'r' or '*' match to the left
+            - From a 'c' or '*' match, move up if 'c' or '*' match up
+
+            >>> matrix = MatchMatrix("a{b}c{d}e", "abccc{e}e")
+            >>> for path in matrix.paths():
+            ...     print path
+            ((0, 0), (1, 1), (1, 2), (1, 3), (2, 4), (3, 5), (4, 6))
+            ((0, 0), (1, 1), (1, 2), (2, 3), (3, 4), (3, 5), (4, 6))
+            ((0, 0), (1, 1), (2, 2), (3, 3), (3, 4), (3, 5), (4, 6))
+            ((0, 0), (1, 1), (1, 2), (1, 3), (1, 4), (2, 5), (3, 5), (4, 6))
+            >>> matrix = MatchMatrix("a{b}{c}def", "abc{d}{e}f")
+            >>> for path in matrix.paths():
+            ...     print path
+            ((0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
+            ((0, 0), (1, 1), (1, 2), (2, 3), (3, 4), (4, 4), (5, 5))
+            ((0, 0), (1, 1), (2, 2), (2, 3), (3, 4), (4, 4), (5, 5))
+        """
+
+        # No path to the end, skip out
+        if self[-1, -1].paths == 0:
+            return
+
+        if path is None:
+            path = (self.num_rows - 1, self.num_cols - 1),
+
+        # We have reached the beginning from the end: emit
+        row, col = path[0]
+        if row == 0 and col == 0:
+            yield path
+            return
+
+        info = self[row, col]
+
+        path_prefixes = []
+        if row > 0 and col > 0 and self[row-1, col-1].paths > 0:
+            path_prefixes.append((row-1, col-1))
+        if info.type in 'r*' and col > 0 and self[row, col-1].type in 'r*':
+            path_prefixes.append((row, col-1))
+        if info.type in 'c*' and row > 0 and self[row-1, col].type in 'c*':
+            path_prefixes.append((row-1, col))
+
+        for pos in path_prefixes:
+            for new_path in self.paths((pos,) + path):
+                yield new_path
+
+    def maps( self ):
+        """ Build a map for each path through this matrix
+
+            >>> matrix = MatchMatrix("a{b}{c}def", "abc{d}{e}f")
+            >>> for map in matrix.maps():
+            ...     print map
+            [['{b}', 'b'], ['{c}', 'c'], ['{d}', 'd'], ['{e}', 'e']]
+            [['{b}', 'b', 'c'], ['{c}', '{d}'], ['{e}', 'd', 'e']]
+            [['{b}', 'b'], ['{c}', 'c', '{d}'], ['{e}', 'd', 'e']]
+            >>> matrix = MatchMatrix("a{b}c{d}e", "abccc{e}e")
+            >>> for map in matrix.maps():
+            ...     print map
+            [['{b}', 'b', 'c', 'c'], ['{d}', '{e}']]
+            [['{b}', 'b', 'c'], ['{d}', 'c', '{e}']]
+            [['{b}', 'b'], ['{d}', 'c', 'c', '{e}']]
+            [['{b}', 'b', 'c', 'c', 'c'], ['{e}', 'c', '{d}']]
+        """
+        for path in self.paths():
+            prev_row = None
+            prev_col = None
+            map = []
+            for i, (row, col) in enumerate(path):
+                # If we move within a row, we need to append to the most recent
+                # row variable match.
+                if row == prev_row:
+                    map[-1].append(self.ps2[col])
+                elif col == prev_col:
+                    map[-1].append(self.ps1[row])
+                else:
+                    # Moving diagonally.  If we have a variable type, create a
+                    # new match.  This is straightforward for 'r' and 'c' type
+                    # matches.
+                    info = self[row, col]
+                    if info.type == 'r':
+                        map.append([self.ps1[row], self.ps2[col]])
+                    elif info.type == 'c':
+                        map.append([self.ps2[col], self.ps1[row]])
+                    elif info.type == '*':
+                        # This is the tricky one.  From here there are three
+                        # possibilities:
+                        #   move down: column var
+                        #   move right: row var
+                        #   move diagonally or end of path: treat like a row var
+                        if i == len(path) - 1:
+                            # end of path, treat like a row var:
+                            map.append([self.ps1[row], self.ps2[col]])
+                        else:
+                            future_row, future_col = path[i+1]
+                            if future_col == col:
+                                map.append([self.ps2[col], self.ps1[row]])
+                            else: # diagonal or over
+                                map.append([self.ps1[row], self.ps2[col]])
+                prev_row = row
+                prev_col = col
+            yield map
+
+    def __iter__( self ):
+        return iter(self.matrix)
+
+    def __getitem__( self, pos ):
+        row, col = pos
+        return self.matrix[row][col]
+
+    def __str__( self ):
+        slist = []
+        for row in self.matrix:
+            slist.append(" ".join(str(x) for x in row))
+        return "\n".join(slist)
+
+    __repr__ = __str__
+
 def parse_match_string(s):
     """ Parse a match string into a list of tokens
 
@@ -68,99 +312,6 @@ def parse_match_string(s):
     if cur_var:
         raise ValueError("Unterminated variable name " + cur_var)
     return result
-
-def iter_string_matches(s1, s2):
-    """ Try to build a variable mapping that unifies the two variable-containing
-        strings.
-
-        A matrix might look something like this:
-
-              0  1  2  3  4  5  6
-              a  b  c  c  c {e} e
-        0  a  1  -  -  -  -  -  -
-        1 {b} 2  2  3  4  5  6  7  
-        2  c  -  -  3  4  5  6  -
-        3 {d} -  -  4  4  5  6  7
-        4  e  -  -  -  -  -  6  7
-
-        All paths through the matrix that begin at 0,0 and end at 4,6 represent
-        a possible match (row,column coordinates).  Each step of the path must
-        increase the cost value by 1.  From a constant (non-var) you can only
-        travel down and to the right.  From a row variable, you can also travel
-        right, and from a column variable, you can also travel left.
-
-        0,0; 1,1; 1,2; 1,3; 2,4; 3,5; 4,6 : {b}=bcc, {d}={e}
-        0,0; 1,1; 1,2; 2,3; 3,4; 3,5; 4,6 : {b}=bc, {d}=c{e}
-
-        etc.
-
-        >>> for vmap in iter_string_matches("a{b}c{d}e", "abccc{e}e"):
-        ...     print vmap
-        None
-    """
-    import sys
-
-    ps1 = parse_match_string(s1)
-    ps2 = parse_match_string(s2)
-
-    def is_var( v ) :
-        return len(v) > 1
-
-    matrix = [[None for col in ps2] for row in ps1]
-    # Start at the upper right corner and make our way down to the lower left.
-    for col, cval in enumerate(ps2):
-        for row, rval in enumerate(ps1):
-            cur_cost = None
-            if is_var(cval) or is_var(rval) or rval == cval:
-                cur_cost = 1
-            if row == col == 0:
-                if cur_cost is None:
-                    # No match at beginning, bail
-                    return
-                else:
-                    matrix[row][col] = cur_cost
-            else:
-                cur_min = None
-                for prow, pcol in [(row-1, col), (row-1, col-1), (row, col-1)]:
-                    if prow < 0 or pcol < 0:
-                        continue
-                    past_val = matrix[prow][pcol]
-                    if cur_min is None:
-                        cur_min = past_val
-                    elif past_val is not None and cur_min > past_val:
-                        cur_min = past_val
-                if cur_min is not None and cur_cost is not None:
-                    matrix[row][col] = cur_min + cur_cost
-
-    if matrix[-1][-1] is None:
-        return
-
-    # Start at the back and move to the beginning, tracing all possible paths
-    row = len(ps1) - 1
-    col = len(ps2) - 1
-
-    stack = [((row, col),)]
-
-    while stack:
-        path = stack.pop()
-        row, col = path[-1]
-        if row == col == 0:
-            # leaf node.  Yield this path.
-            # TODO: transform the path into a variable map, then yield that.
-            yield path
-            continue
-        # Try to push every neigbor onto the stack, but only if its cost is
-        # equal to this cost - 1, and the following conditions are met:
-        # If row is a var: can travel left
-        # If col is a var: can travel up
-        # Always: can travel diagonally up-left
-        cost = matrix[row][col]
-        # TODO: Fix to only travel in possible directions
-        for nrow, ncol in ((row-1, col), (row-1, col-1), (row, col-1)):
-            if nrow < 0 or ncol < 0:
-                continue
-            if matrix[nrow][ncol] == cost - 1:
-                stack.append(path + ((nrow, ncol),))
 
 def prepare_func(func):
     if not hasattr(func, 'provides'):
